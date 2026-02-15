165e85924a56b0b1d6efe22e395b43d876ff0e81
SUlTlUS <109349443+SUlTlUS@users.noreply.github.com>
2026-02-15 22:35:40 +0800

    Update TianYinWallpaperService.java

--- app/src/main/java/com/zeaze/tianyinwallpaper/service/TianYinWallpaperService.java
+++ app/src/main/java/com/zeaze/tianyinwallpaper/service/TianYinWallpaperService.java
@@ -83,6 +83,7 @@ public class TianYinWallpaperService extends WallpaperService {
                 if (eglThread != null) eglThread.requestRender();
             } else {
                 if (mediaPlayer != null && mediaPlayer.isPlaying()) mediaPlayer.pause();
+                nextWallpaper();
             }
         }
 
