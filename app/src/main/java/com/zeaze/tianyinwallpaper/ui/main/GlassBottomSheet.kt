7a01d5bd22c186c39ed94b17d192dec32840094f
SUlTlUS <109349443+SUlTlUS@users.noreply.github.com>
2026-02-21 09:25:39 +0800

    更新 GlassBottomSheet.kt

--- app/src/main/java/com/zeaze/tianyinwallpaper/ui/main/GlassBottomSheet.kt
+++ app/src/main/java/com/zeaze/tianyinwallpaper/ui/main/GlassBottomSheet.kt
@@ -1,21 +1,22 @@
-// GlassBottomSheet.kt
 package com.zeaze.tianyinwallpaper.ui.main
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kyant.backdrop.Backdrop
 import com.kyant.backdrop.drawBackdrop
 import com.kyant.backdrop.effects.blur
