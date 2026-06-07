ea9eabecd181402714be4901657ac1ee38488b77
MiHN <109349443+SUlTlUS@users.noreply.github.com>
2026-06-07 13:04:21 +0800

    Fix depth prefs synchronous persistence

--- app/src/main/java/com/zeaze/tianyinwallpaper/utils/DepthPrefs.kt
+++ app/src/main/java/com/zeaze/tianyinwallpaper/utils/DepthPrefs.kt
@@ -1,5 +1,12 @@
 package com.zeaze.tianyinwallpaper.utils
+
 import android.content.SharedPreferences
 import com.alibaba.fastjson.JSON
 import com.zeaze.tianyinwallpaper.model.DepthWallpaperModel
-object DepthPrefs{const val PREF_DEPTH_WALLPAPERS="depthWallpapers";const val PREF_DEPTH_ACTIVE_ID="depthActiveWallpaperId";fun loadWallpapers(p:SharedPreferences)=try{JSON.parseArray(p.getString(PREF_DEPTH_WALLPAPERS,"[]"),DepthWallpaperModel::class.java)
+
+object DepthPrefs {
+    const val PREF_DEPTH_WALLPAPERS = "depthWallpapers"
+    const val PREF_DEPTH_ACTIVE_ID = "depthActiveWallpaperId"
+
+    fun loadWallpapers(pref: SharedPreferences): List<DepthWallpaperModel> {
+        return run
