78a218831cac61322aae1d2312f2c3dd9854e2e3
MiHN <109349443+SUlTlUS@users.noreply.github.com>
2026-06-07 13:04:58 +0800

    Restore DepthPrefs and commit changes synchronously

--- app/src/main/java/com/zeaze/tianyinwallpaper/utils/DepthPrefs.kt
+++ app/src/main/java/com/zeaze/tianyinwallpaper/utils/DepthPrefs.kt
@@ -1,12 +1,5 @@
 package com.zeaze.tianyinwallpaper.utils
-
 import android.content.SharedPreferences
 import com.alibaba.fastjson.JSON
 import com.zeaze.tianyinwallpaper.model.DepthWallpaperModel
-
-object DepthPrefs {
-    const val PREF_DEPTH_WALLPAPERS = "depthWallpapers"
-    const val PREF_DEPTH_ACTIVE_ID = "depthActiveWallpaperId"
-
-    fun loadWallpapers(pref: SharedPreferences): List<DepthWallpaperModel> {
-        return run
+object DepthPrefs{const val PREF_DEPTH_WALLPAPERS="depthWallpapers";const val PREF_DEPTH_ACTIVE_ID="depthActiveWallpaperId";fun loadWallpapers(pref:SharedPreferences):List<DepthWallpaperModel>{return try{JSON.parseArray(pref.getString(PREF_DEPTH_WALLPAPERS,"[]"),DepthWallpaperModel::
