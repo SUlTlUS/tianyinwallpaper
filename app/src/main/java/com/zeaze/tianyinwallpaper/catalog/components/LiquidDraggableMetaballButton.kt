0cb3929b5e78dc91c52762065383c664b04fbbd8
Shadowfyre <shadowfyre@foxmail.com>
2026-06-11 10:48:21 +0800

    Upgrade Compose dependencies

--- app/src/main/java/com/zeaze/tianyinwallpaper/catalog/components/LiquidDraggableMetaballButton.kt
+++ app/src/main/java/com/zeaze/tianyinwallpaper/catalog/components/LiquidDraggableMetaballButton.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
